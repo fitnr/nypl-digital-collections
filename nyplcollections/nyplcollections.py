e920cb1e84b3cb0f9152abd15e54fc55ff7b2abf
fitnr <fitnr@fakeisthenewreal>
2015-02-12 13:45:25 -0500

    make actual results array the return value

--- nyplcollections/nyplcollections.py
+++ nyplcollections/nyplcollections.py
@@ -6,6 +6,15 @@ import xmltodict
 def stop():
     raise StopIteration
 
+def _result_picker(key, results):
+    n = int(results['numResults'])
+    if n == 0:
+        return []
+    elif n == 1:
+        return [results[key]]
+    else:
+        return results[key]
+
 class NYPLsearch(object):
     raw_results = ''
     headers = dict()
@@ -24,25 +33,29 @@ class NYPLsearch(object):
     # optional value withTitles=yes
     def captures(self, uuid, withTitles=False):
         url = '/'.join([self.base, uuid])
-        return self._get(url, withTitles='yes' if withTitles else 'no')
+        picker = lambda x: x.get('capture', [])
+        return self._get(url, picker, withTitles='yes' if withTitles else 'no')
 
     # Return the item-uuid for a identifier.
     def uuid(self, type, val):
-        return self._get('/'.join((self.base, type, val)))
+        picker = lambda x: _result_picker('uuid', x)
+        return self._get('/'.join((self.base, type, val)), picker)
 
     # Search across all (without field) or in specific field
     # (valid fields at http://www.loc.gov/standards/mods/mods-outline.html)
     def search(self, q, field=None, page=None, per_page=None):
         url = '/'.join((self.base, 'search'))
-        return self._get(url, q=q, field=field, page=page, per_page=per_page)
+        picker = lambda x: _result_picker('result', x)
+        return self._get(url, picker, q=q, field=field, page=page, per_page=per_page)
 
     # Return a mods record for a given uuid
     def mods(self, uuid):
-        return self._get('/'.join((self.base, 'mods', uuid)))
+        picker = lambda x: x.get('mods')
+        return self._get('/'.join((self.base, 'mods', uuid)), picker)
 
     # Generic get which handles call to api and setting of results
     # Return: results dict
-    def _get(self, url, **params):
+    def _get(self, url, picker, **params):
         self.raw_results = self.results = None
 
         headers = {"Authorization": "Token token=" + self.token}
@@ -74,7 +87,7 @@ class NYPLsearch(object):
         else:
             self.error = None
 
-        return self.results
+        return picker(results)
 
     def _to_dict(self, r):
         return r.json() if self.format == 'json' else xmltodict.parse(r.text)
